34d450a9fd5d1d791b00ed112cde3e24d3f3ce08
nvminhtam <nvminhtam2201@gmail.com>
2022-01-12 22:15:28 +0700

    change db connect

--- models/index.js
+++ models/index.js
@@ -5,11 +5,15 @@ const initModels = require("./init-models");
 //     timezone: '+07:00'
 // });
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     timezone: '+07:00'
+// });
+const sequelize = new Sequelize("mysql://doadmin:07TZPIrkKqJrHQ0Q@db-mysql-sgp1-23535-do-user-9572625-0.b.db.ondigitalocean.com:25060/NMCNPM?ssl-mode=REQUIRED", {
     timezone: '+07:00'
 });
+
 module.exports = {
     sequelize,
     models: initModels(sequelize)
