7b9ecf4f1764604c232f5f022bc6006c8b3b2b9c
nvminhtam <nvminhtam2201@gmail.com>
2022-01-12 22:14:15 +0700

    add flight

--- models/index.js
+++ models/index.js
@@ -1,13 +1,15 @@
 const Sequelize = require('sequelize');
 const initModels = require("./init-models");
 
-// const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     timezone: '+07:00'
+// });
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+07:00'
 });
-
 module.exports = {
     sequelize,
     models: initModels(sequelize)
