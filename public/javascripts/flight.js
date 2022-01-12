c0358d93ca2659ca13e2f906453a91e1650738fe
nvminhtam <nvminhtam2201@gmail.com>
2022-01-12 10:41:28 +0700

    add flight

--- public/javascripts/flight.js
+++ public/javascripts/flight.js
@@ -1,14 +1,37 @@
+const config = {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today",
+    time_24hr: true,
+    enableSeconds: true
+}
 jQuery(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.location = $(this).data("href");
     });
-    // $('.datepicker').datepicker({ "format": 'dd-mm-yyyy' }).datepicker("setDate", new Date()).datepicker({ "autoClose": true });
-    $("#departureDatePicker").datepicker({
-        autoclose: true,
-        todayHighlight: true
-    }).datepicker('update', new Date());
-    $("#arrivalDatePicker").datepicker({
-        autoclose: true,
-        todayHighlight: true
-    }).datepicker('update', new Date());
+
+    flatpickr("#departureTime", config);
+    flatpickr("#arrivalTime", config);
 });
+var path = window.location.pathname;
+if (path.match('/flights/addflight')) {
+    // readSingleFile()
+
+    // function readSingleFile(evt) {
+    //     //Retrieve the first (and only!) File from the FileList object
+    //     var f = evt.target.files[0];
+
+    //     if (f) {
+    //         var r = new FileReader();
+    //         r.onload = function(e) {
+    //             var contents = e.target.result;
+    //             document.getElementById("ReadResult").innerHTML = contents;
+    //         }
+    //         r.readAsText(f);
+    //     } else {
+    //         alert("Failed to load file");
+    //     }
+    // }
+
+    // document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
+}
